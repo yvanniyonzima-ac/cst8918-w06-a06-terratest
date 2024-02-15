6a2f9512c3e9ecf17facc0cc3342f8315cd6a5ff
Robert McKenney <robert@mckennehy.ca>
2024-02-15 14:15:06 -0500

    ADD output values to support testing

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,14 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.webserver.name
+}
+
+output "nic_name" {
+  value = azurerm_network_interface.webserver.name
+}
+
 output "public_ip" {
   value = azurerm_linux_virtual_machine.webserver.public_ip_address
 }
